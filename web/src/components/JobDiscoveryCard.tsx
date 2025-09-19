e43e14cd905e2896416eda6a88f0b6bd6f55b162
GoGoPenguin <gogopenguin1995@gmail.com>
2025-09-19 17:35:06 +0800

    refactor: :lipstick: enhance JobDiscoveryCard layout for better responsiveness and readability

--- web/src/components/JobDiscoveryCard.tsx
+++ web/src/components/JobDiscoveryCard.tsx
@@ -48,7 +48,11 @@ export default function JobDiscoveryCard({ job }: { job: Job }) {
         }),
       })}
     >
-      <Stack spacing={2} sx={{ width: "100%" }}>
+      <Stack
+        spacing={2}
+        sx={{ width: "100%" }}
+        display={{ xs: "none", md: "flex" }}
+      >
         <Stack direction="row" spacing={2} alignItems="center">
           <Stack direction="column">
             <LetterAvatar
@@ -106,8 +110,8 @@ export default function JobDiscoveryCard({ job }: { job: Job }) {
                 </Stack>
                 <Stack
                   mt={1}
-                  direction={{ xs: "column", sm: "row" }}
-                  spacing={{ xs: 0, sm: 1 }}
+                  direction="row"
+                  spacing={1}
                   color="text.secondary"
                   divider={<Divider orientation="vertical" flexItem />}
                 >
@@ -158,7 +162,7 @@ export default function JobDiscoveryCard({ job }: { job: Job }) {
                   direction="row"
                   spacing={1}
                   alignItems="center"
-                  sx={{ display: { xs: "none", md: "flex" } }}
+                  display="flex"
                 >
                   <Typography variant="caption" color="text.secondary">
                     {t("job.postedAt", {
@@ -198,31 +202,142 @@ export default function JobDiscoveryCard({ job }: { job: Job }) {
             </Stack>
           </Stack>
         </Stack>
-        <Stack direction="column" sx={{ display: { xs: "flex", md: "none" } }}>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography variant="caption" color="text.secondary">
-              {t("job.postedAt", {
-                timeAgo: t("time.hour", { count: 2 }),
-              })}
-            </Typography>
+      </Stack>
+      <Stack
+        spacing={2}
+        sx={{ width: "100%" }}
+        display={{ xs: "flex", md: "none" }}
+      >
+        <Stack direction="row" spacing={2} alignItems="center">
+          <Stack direction="column">
+            <LetterAvatar
+              variant="rounded"
+              name={job.company}
+              sx={{ width: 56, height: 56 }}
+            ></LetterAvatar>
           </Stack>
-          <Stack direction="row" justifyContent="space-between" spacing={1}>
-            <Stack direction="column" flex={1}>
-              <Button
-                variant="outlined"
-                size="small"
-                color="primary"
-                startIcon={<BookmarkBorderOutlinedIcon />}
+          <Stack direction="column">
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Typography
+                variant="h5"
+                component="h3"
+                sx={{ fontWeight: "bold" }}
               >
-                {t("job.save")}
-              </Button>
-            </Stack>
-            <Stack direction="column" flex={3}>
-              <Button variant="contained" size="small" color="primary">
-                {t("job.apply")}
-              </Button>
+                {job.title}
+              </Typography>
+              <Chip
+                label={t(`job.${job.type.toLowerCase()}`)}
+                size="small"
+                variant="filled"
+                sx={(theme) => ({
+                  mt: 0.5,
+                  bgcolor:
+                    theme.palette.mode === "dark" ? "grey.700" : "grey.300",
+                  fontWeight: "bold",
+                })}
+              />
             </Stack>
           </Stack>
+        </Stack>
+        <Stack direction="row">
+          <Tooltip title={job.notes}>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                maxWidth: 400,
+                display: "-webkit-box",
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "normal",
+              }}
+            >
+              {job.notes}
+            </Typography>
+          </Tooltip>
+        </Stack>
+        <Stack direction="row">
+          <Stack direction="column" flex={1}>
+            <Stack direction="row" spacing={1}>
+              <Tooltip title={t("job.vacancies")}>
+                <PersonOutline fontSize="small" />
+              </Tooltip>
+              <Typography variant="caption">{job.vacancies}</Typography>
+            </Stack>
+            <Stack direction="row" spacing={1}>
+              <Tooltip title={t("job.location")}>
+                <LocationOnOutlined fontSize="small" />
+              </Tooltip>
+              <Typography variant="caption">
+                {t(`cities.${job.location}`)}
+              </Typography>
+            </Stack>
+            <Stack direction="row" spacing={1}>
+              <Tooltip title={t("job.date")}>
+                <EventIcon fontSize="small" />
+              </Tooltip>
+              <Typography variant="caption">
+                {new Date(job.date).toLocaleDateString("en-CA")}
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction="column" flex={1}>
+            <Stack direction="row" spacing={1}>
+              <Tooltip title={t("job.hours")}>
+                <AccessTimeOutlinedIcon fontSize="small" />
+              </Tooltip>
+              <Typography variant="caption">
+                {t("time.hour", { count: job.hours })}
+              </Typography>
+            </Stack>
+            <Stack direction="row" spacing={1}>
+              <Tooltip title={t("job.wage")}>
+                <MonetizationOnOutlined fontSize="small" />
+              </Tooltip>
+              <Typography variant="caption">
+                {Intl.NumberFormat(language, {
+                  currency: "TWD",
+                }).format(job.wage)}
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack direction="column" spacing={1}>
+          <Typography variant="caption" color="text.secondary">
+            {t("job.postedAt", {
+              timeAgo:
+                postedAtYears > 0
+                  ? t("time.year", { count: postedAtYears })
+                  : postedAtMonths > 0
+                  ? t("time.month", { count: postedAtMonths })
+                  : postedAtDays > 0
+                  ? t("time.day", { count: postedAtDays })
+                  : postedAtHours > 0
+                  ? t("time.hour", {
+                      count: Math.max(postedAtHours, 1),
+                    })
+                  : postedAtMinutes > 0
+                  ? t("time.minute", {
+                      count: Math.max(postedAtMinutes, 1),
+                    })
+                  : t("time.second", {
+                      count: Math.max(postedAtSeconds, 1),
+                    }),
+            })}
+          </Typography>
+          <Button
+            variant="outlined"
+            size="small"
+            color="primary"
+            startIcon={<BookmarkBorderOutlinedIcon />}
+          >
+            {t("job.save")}
+          </Button>
+          <Button variant="contained" size="small" color="primary" fullWidth>
+            {t("job.apply")}
+          </Button>
         </Stack>
       </Stack>
     </Paper>
