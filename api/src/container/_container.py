e0be339169c1ac7cec58c830958eb54dffa010f4
GoGoPenguin <gogopenguin1995@gmail.com>
2025-10-10 16:52:20 +0800

    feat: :card_file_box: update database options in .env.template and enhance Container class for MongoDB connection

--- api/src/container/_container.py
+++ api/src/container/_container.py
@@ -1,7 +1,20 @@
 from dependency_injector import containers, providers
+from mongoengine import connect
 from src.utils.config import Config
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
     config.from_pydantic(Config())  # type: ignore
+
+    db = providers.Resource(
+        connect,
+        host="mongodb://{user}:{password}@{host}:{port}/{database}?{options}".format(
+            user=config.db.user(),
+            password=config.db.password().get_secret_value(),
+            host=config.db.host(),
+            port=config.db.port(),
+            database=config.db.database(),
+            options=config.db.options(),
+        ),
+    )
