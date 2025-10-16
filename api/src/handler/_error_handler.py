b54ac9c528d81f6a607729ae94ae6a15afa8745e
GoGoPenguin <gogopenguin1995@gmail.com>
2025-10-16 14:22:49 +0800

    fix(api): :lock: consolidate headers in error handler for cors issues

--- api/src/handler/_error_handler.py
+++ api/src/handler/_error_handler.py
@@ -11,50 +11,47 @@ from src.errors._errors import ConflictError
 
 
 async def error_handler(request: Request, ex: Exception):
+    headers = {
+        "Content-Type": "application/problem+json",
+        # HACK: Allowing all origins as we cannot determine the correct origin here
+        #       without risking CORS issues.
+        "Access-Control-Allow-Origin": request.headers.get("origin", "*"),
+        "Access-Control-Allow-Credentials": "true",
+    }
     match ex:
         case Error():
             err = cast(Error, ex)
             err.instance = request.url.path
             return ORJSONResponse(
-                headers={
-                    "Content-Type": "application/problem+json",
-                },
+                headers=headers,
                 content=err.model_dump(),
                 status_code=err.status,
             )
         case PyJWTError():
             err = UnauthorizedError()
             return ORJSONResponse(
-                headers={
-                    "Content-Type": "application/problem+json",
-                },
+                headers=headers,
                 content=err.model_dump(),
                 status_code=err.status,
             )
         case NotUniqueError():
             err = ConflictError(instance=request.url.path)
             return ORJSONResponse(
-                headers={
-                    "Content-Type": "application/problem+json",
-                },
+                headers=headers,
                 content=err.model_dump(),
                 status_code=err.status,
             )
         case RequestValidationError():
             err = ValidationError(instance=request.url.path, errors=ex)
             return ORJSONResponse(
-                headers={
-                    "Content-Type": "application/problem+json",
-                },
+                headers=headers,
                 content=err.model_dump(),
                 status_code=err.status,
             )
         case _:
             err = InternalServerError(instance=request.url.path, detail=str(ex))
             return ORJSONResponse(
-                headers={
-                    "Content-Type": "application/problem+json",
-                },
+                headers=headers,
                 content=err.model_dump(),
                 status_code=err.status,
             )
