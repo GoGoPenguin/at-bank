e676fbacad2ca5e299cd630b7032e63ed28a869c
GoGoPenguin <gogopenguin1995@gmail.com>
2025-10-16 15:33:07 +0800

    fix(api): :bug: update GetJobHandler to return specific job schemas based on job type

--- api/src/handler/_get_job_handler.py
+++ api/src/handler/_get_job_handler.py
@@ -1,14 +1,23 @@
-from typing import Optional
+from typing import Union
 
 from dependency_injector.wiring import Provide
 
-from src.schema import JobSchema
+from src.document import Department, TournamentJob
+from src.schema import DepartmentJobSchema, IndividualJobSchema, TournamentJobSchema
 from src.service import JobService
 
 
 class GetJobHandler:
     job_service: JobService = Provide["job_service"]
 
-    def handle(self, id: str) -> Optional[JobSchema]:
+    def handle(
+        self, id: str
+    ) -> Union[TournamentJobSchema, DepartmentJobSchema, IndividualJobSchema]:
         job = self.job_service.get_job(id)
-        return JobSchema(**job.to_dict())
+        return (
+            TournamentJobSchema(**job.to_dict())
+            if isinstance(job, TournamentJob)
+            else DepartmentJobSchema(**job.to_dict())
+            if isinstance(job, Department)
+            else IndividualJobSchema(**job.to_dict())
+        )
