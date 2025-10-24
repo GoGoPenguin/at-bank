ef57b18aeeadb5831e11f00e4c8881313ebdae79
GoGoPenguin <gogopenguin1995@gmail.com>
2025-10-24 15:22:24 +0800

    fix(api): :goal_net: Add NotFoundError handling for job retrieval in update job status handler

--- api/src/handler/_update_job_status_handler.py
+++ api/src/handler/_update_job_status_handler.py
@@ -1,8 +1,10 @@
+from typing import cast
+
 from dependency_injector.wiring import Provide
 from fastapi import Request
 
 from src.document import Department
-from src.errors import ForbiddenError
+from src.errors import ForbiddenError, NotFoundError
 from src.schema import JobSchema, UpdateJobStatusSchema
 from src.service import JobService
 
@@ -13,6 +15,14 @@ class UpdateJobStatusHandler:
     def handle(self, params: UpdateJobStatusSchema, request: Request) -> JobSchema:
         if not isinstance(request.state.user, Department):
             raise ForbiddenError(detail="Only departments can update job status.")
+
+        job = self.job_service.get_job(params.job_id)
+        if not job:
+            raise NotFoundError(detail="Job not found.")
+        if cast(Department, job.created_by).id != request.state.user.id:
+            raise ForbiddenError(
+                detail="You do not have permission to update this job."
+            )
 
         job = self.job_service.update_job_status(params.job_id, params.status)
         return JobSchema(**job.to_dict())
