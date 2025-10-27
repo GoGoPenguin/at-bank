7727cde845834bc871cdffda195ac6f5830c9f2d
GoGoPenguin <gogopenguin1995@gmail.com>
2025-10-28 00:37:22 +0800

    feat(web): :sparkles: Refactor application record table to use collapsible rows for detailed view and improve status update handling

--- web/src/pages/ApplicationRecord.tsx
+++ web/src/pages/ApplicationRecord.tsx
@@ -1,5 +1,9 @@
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import {
   Button,
+  Collapse,
+  IconButton,
   Stack,
   Table,
   TableBody,
@@ -7,11 +11,12 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import { useQuery } from "@tanstack/react-query";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Container from "../components/Container";
 import Footer from "../components/Footer";
@@ -20,23 +25,16 @@ import alertContext from "../context/alert.context";
 import useApi from "../hooks/use-api.hook";
 import { useAuthStore } from "../store/use-auth.store";
 import {
+  type JobApplication,
   APPLICATION_STATUS_ACCEPTED,
   APPLICATION_STATUS_REJECTED,
 } from "../types/job.type";
 
-function DepartmentHome() {
+function Row(props: { row: JobApplication }) {
   const { t } = useTranslation();
-  const { setUser } = useAuthStore();
-  const { getApplications, getMe, updateApplicationStatus } = useApi();
+  const { updateApplicationStatus } = useApi();
+  const [open, setOpen] = useState(false);
   const { handleAlert } = useContext(alertContext);
-  const { data: applications, refetch } = useQuery({
-    queryKey: [`applications`],
-    queryFn: () => getApplications(),
-  });
-  const { data: user, isSuccess } = useQuery({
-    queryKey: ["getMe"],
-    queryFn: getMe,
-  });
 
   const handleApprove = async (applicationId: string) => {
     await updateApplicationStatus({
@@ -44,7 +42,7 @@ function DepartmentHome() {
       status: APPLICATION_STATUS_ACCEPTED,
     });
     handleAlert("applicationStatusUpdated", "success");
-    refetch();
+    props.row.status = APPLICATION_STATUS_ACCEPTED;
   };
   const handleReject = async (applicationId: string) => {
     await updateApplicationStatus({
@@ -52,8 +50,127 @@ function DepartmentHome() {
       status: APPLICATION_STATUS_REJECTED,
     });
     handleAlert("applicationStatusUpdated", "success");
-    refetch();
+    props.row.status = APPLICATION_STATUS_REJECTED;
   };
+
+  return (
+    <>
+      <TableRow
+        key={props.row.id}
+        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+            sx={{
+              border: "none",
+            }}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>{props.row.job.title}</TableCell>
+        <TableCell align="right">{props.row.applicant.chineseName}</TableCell>
+        <TableCell align="right">{t(`job.${props.row.status}`)}</TableCell>
+        <TableCell align="right">{`${props.row.availableSlots}`}</TableCell>
+        <TableCell>
+          {new Date(props.row.createdAt).toLocaleDateString()}
+        </TableCell>
+        <TableCell>
+          <Stack spacing={1} direction="row">
+            <Button
+              variant="contained"
+              onClick={() => handleApprove(props.row.id)}
+            >
+              {t("departmentApplicationRecord.approve")}
+            </Button>
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={() => handleReject(props.row.id)}
+            >
+              {t("departmentApplicationRecord.reject")}
+            </Button>
+          </Stack>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ marginLeft: 10, marginTop: 2, marginBottom: 2 }}>
+              <Typography variant="caption" gutterBottom component="div">
+                {t("departmentApplicationRecord.applicationDetails")}
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      {t("departmentApplicationRecord.phone")}
+                    </TableCell>
+                    <TableCell>
+                      {t("departmentApplicationRecord.email")}
+                    </TableCell>
+                    <TableCell>
+                      {t("departmentApplicationRecord.emtLicense")}
+                    </TableCell>
+                    <TableCell>
+                      {t("departmentApplicationRecord.emtLicenseValidUntil")}
+                    </TableCell>
+                    <TableCell>
+                      {t("departmentApplicationRecord.tatsLicenseNumber")}
+                    </TableCell>
+                    <TableCell>
+                      {t("departmentApplicationRecord.tatsLicenseValidUntil")}
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow key={props.row.id}>
+                    <TableCell>{props.row.applicant.phone}</TableCell>
+                    <TableCell>{props.row.applicant.email}</TableCell>
+                    <TableCell>{props.row.applicant.emtLicense}</TableCell>
+                    <TableCell>
+                      {props.row.applicant.emtLicenseValidUntil
+                        ? new Date(
+                            props.row.applicant.emtLicenseValidUntil
+                          ).toLocaleDateString()
+                        : ""}
+                    </TableCell>
+                    <TableCell>
+                      {props.row.applicant.tatsLicenseNumber}
+                    </TableCell>
+                    <TableCell>
+                      {props.row.applicant.tatsLicenseValidUntil
+                        ? new Date(
+                            props.row.applicant.tatsLicenseValidUntil
+                          ).toLocaleDateString()
+                        : ""}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
+
+export default function ApplicationRecord() {
+  const { t } = useTranslation();
+  const { setUser } = useAuthStore();
+  const { getApplications, getMe } = useApi();
+  const { data: applications } = useQuery({
+    queryKey: [`applications`],
+    queryFn: () => getApplications(),
+  });
+  const { data: user, isSuccess } = useQuery({
+    queryKey: ["getMe"],
+    queryFn: getMe,
+  });
 
   useEffect(() => {
     if (isSuccess) {
@@ -94,44 +211,15 @@ function DepartmentHome() {
                 <TableCell align="right">
                   {t("departmentApplicationRecord.availableSlots")}
                 </TableCell>
+                <TableCell align="right">
+                  {t("departmentApplicationRecord.applicationDate")}
+                </TableCell>
                 <TableCell />
               </TableRow>
             </TableHead>
             <TableBody>
-              {applications?.map((application, index) => (
-                <TableRow
-                  key={application.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">{`#${
-                    index + 1
-                  }`}</TableCell>
-                  <TableCell>{application.job.title}</TableCell>
-                  <TableCell align="right">
-                    {application.applicant.chineseName}
-                  </TableCell>
-                  <TableCell align="right">
-                    {t(`job.${application.status}`)}
-                  </TableCell>
-                  <TableCell align="right">{`${application.availableSlots}`}</TableCell>
-                  <TableCell>
-                    <Stack spacing={1} direction="row">
-                      <Button
-                        variant="contained"
-                        onClick={() => handleApprove(application.id)}
-                      >
-                        {t("departmentApplicationRecord.approve")}
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        color="error"
-                        onClick={() => handleReject(application.id)}
-                      >
-                        {t("departmentApplicationRecord.reject")}
-                      </Button>
-                    </Stack>
-                  </TableCell>
-                </TableRow>
+              {applications?.map((application) => (
+                <Row key={application.id} row={application} />
               ))}
             </TableBody>
           </Table>
@@ -142,5 +230,3 @@ function DepartmentHome() {
     </>
   );
 }
-
-export default DepartmentHome;
